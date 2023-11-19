11243e667189f6ee4d96a65e012e9974fbf2cee8
Ziya Memmedli <ziyarma@code.edu.az>
2023-11-19 18:20:17 +0400

    my homework

---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿#region task1 : aylar, fesiller
+
+Console.WriteLine("ayi daxi et");string text = Convert.ToString(Console.ReadLine());
+
+if (text == "dekabr" || text=="yanvar" || text == "fevral")
+{
+    Console.WriteLine("qis feslidir");
+}
+else if (text == "mart" || text == "aprel" || text == "may")
+{
+    Console.WriteLine("yaz feslidir");
+}
+else if (text == "iyun" || text =="iyul" || text=="avqust")
+{
+    Console.WriteLine("yay feslidir");
+}
+else if (text == "sentyabr" || text=="oktyabr" || text=="noyabr")
+{
+    Console.WriteLine("payiz feslidir");
+}
+else
+{
+    Console.WriteLine("yanlis ad");
+}
+#endregion
+
+
+
+#region task3 : faktorial
+//Console.WriteLine("ededi daxil et");var num = Convert.ToInt32(Console.ReadLine());
+//var faktorial = 1;
+//var start = 1;
+//for (start = 1; start <= num; start++)
+//{
+//    faktorial = faktorial * start;
+//}
+//    Console.WriteLine("ededin faktoriali-----"+faktorial);
+
+
+
+
+#endregion
+
+
+
+
+
+#region task2 : ededin mertebelerinin sayi
+//Console.WriteLine("ededi daxil edin");long num=Convert.ToInt64(Console.ReadLine());
+
+//if(num>=0 && num <= 9)
+//{
+//    Console.WriteLine("1");
+//}
+//else if(num>9 && num <= 99)
+//{
+//    Console.WriteLine("2");
+//}
+//else if(num>99 && num <= 999)
+//{
+//    Console.WriteLine("3");
+//}
+//else if(num>999 && num <= 9999)
+//{
+//   Console.WriteLine("4");
+//}
+//else if(num>9999 && num <= 99999)
+//{
+//    Console.WriteLine("5");
+//}
+//else if(num>99999 && num <= 999999)
+//{
+//    Console.WriteLine("6");
+//}
+//else if(num>999999 && num <= 9999999)
+//{
+//    Console.WriteLine("7");
+//}
+//else if(num>9999999 && num <= 99999999)
+//{
+//    Console.WriteLine("8");
+//}
+//else if(num>99999999 && num <= 999999999)
+//{
+//    Console.WriteLine("9");
+//}
+//else
+//{
+//    Console.WriteLine("invalid number");
+//}
+
+#endregion
+
+
+
+
+
+
+
+
+
+
+
