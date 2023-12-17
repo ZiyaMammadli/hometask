81f87660f590ba04b5612ad45b565c0f3f6d2a56
Ziya Memmedli <ziyarma@code.edu.az>
2023-12-17 11:23:00 +0400

    first commit

---
+++ generic-constraint/Program.cs
@@ -0,0 +1,10 @@
+﻿using generic_constraint.models;
+
+MyClass <string> p1 = new();
+p1.Add("Salam");
+p1.Add("Necesen");
+p1.Add("neyniyirsen");
+p1.Add("ziya");
+p1.Add("memmedli");
+p1.Remove("Salam");
+p1.ShowAll();
---
+++ generic-constraint/models/MyClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace generic_constraint.models
+{
+	public class MyClass<T>
+	{
+		private T[] Arr;
+		public MyClass()
+		{
+			Arr = new T[0];
+		}
+		public void Add(T obj)
+		{
+			Array.Resize(ref Arr, Arr.Length + 1);
+			Arr[^1] = obj;
+		}
+		public void ShowAll()
+		{
+			foreach (var item in Arr)
+			{
+				Console.WriteLine(item);		
+			}
+			Console.WriteLine(Arr.Length);
+		}
+		public int FindIndexOF(T obj)
+		{
+			for(int i = 0; i < Arr.Length; i++)
+			{
+				if (obj.Equals(Arr[i]))
+				{
+					return i;
+				}				
+			}
+			return -1;
+		}
+		public void Remove(T obj)
+		{
+			for(int i = 0; i <= Arr.Length-1; i++)
+			{
+				if (obj.Equals(Arr[i]))
+				{
+					Arr[i] = default;
+					for(int j = i; j < Arr.Length - 1; j++)
+					{
+						Arr[j] = Arr[j + 1];
+					}
+						Array.Resize(ref Arr, Arr.Length - 1);
+				}				
+			}
+		}
+	}
+}
+
